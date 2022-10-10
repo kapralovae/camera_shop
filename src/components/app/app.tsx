e95e387fb128b422f5f7a944a0825df6db601480
kapralovae <kapralov.ae@bk.ru>
2022-10-10 23:51:34 +0300

    setting project + routing 2 page + header

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Catalog from '../../pages/catalog/catalog';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Catalog />}
+        />
+
+        <Route
+          path='/catalog'
+          element={<Catalog />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  SignIn = '/login',
+  MyList = '/mylist',
+  Films = '/films',
+  Root = '/',
+}
