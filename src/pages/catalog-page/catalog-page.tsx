21bfce20c67a162107bdf844cfc0902177f31d65
kapralovae <kapralov.ae@bk.ru>
2022-11-04 21:13:12 +0300

    зпмена в названии попоап '_' -> '-'

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -2,8 +2,8 @@ import Banner from '../../components/banner/banner';
 import Catalog from '../../components/catalog/catalog';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
-import PopupAddInBasket from '../../components/popup_add_in_basket/popup_add_in_basket';
-import PopupAddSuccess from '../../components/popup_add_success/popup_add_success';
+import PopupAddInBasket from '../../components/popup-add-in-basket/popup-add-in-basket';
+import PopupAddSuccess from '../../components/popup-add-success/popup-add-success';
 import { useAppSelector } from '../../hooks';
 import { getIsBasketSuccess } from '../../store/camera-data/selectors';
 
