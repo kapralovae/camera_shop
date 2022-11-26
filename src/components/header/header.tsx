d31606e8d9816fda5bdacbeed6730caca88bfe05
kapralovae <kapralov.ae@bk.ru>
2022-11-26 18:32:16 +0300

    search cameras

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,103 @@
+import { ChangeEvent, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+import { getCameras } from '../../store/camera-process/selecrots';
+import { Cameras } from '../../types/camera';
 
 function Header () {
+  const camerasAll = useAppSelector(getCameras);
+
+  const [value, setValue] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const filteredCameras = (cameras: Cameras, textFilter: string) => cameras.filter((camera) => camera.name.toLocaleLowerCase().includes(textFilter.toLocaleLowerCase()));
+
+  const [style, setStyle] = useState({
+    visibility: 'hidden',
+    opacity: 0,
+  } as React.CSSProperties);
+
+  const [styleButton, setStyleButton] = useState({
+    display: 'none',
+  } as React.CSSProperties);
+
+
+  const handlerOnFocusInput = (evt: ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.value === '') {
+      setStyleButton({
+        display: 'none',
+      });
+    } else {
+      setStyleButton({
+        display: 'block',
+      });
+    }
+  };
+  // const handlerOnBlurInput = () => {
+  //   setStyle({
+  //     visibility: 'hidden',
+  //     opacity: 0,
+  //   });
+  //   setStyleButton({
+  //     display: 'none',
+  //   });
+  // };
+
+
+  const handlerInputChangeText = (evt: ChangeEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    setValue(evt.target.value);
+    if (evt.target.value === '') {
+      setStyle({
+        visibility: 'hidden',
+        opacity: 0,
+      });
+      setStyleButton({
+        display: 'none',
+      });
+    } else {
+      setStyleButton({
+        display: 'block',
+      });
+      setStyle({
+        visibility: 'visible',
+        opacity: 1,
+      });
+    }
+  };
+
+  const handlerClickResetSearchButton = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    setValue('');
+    if (inputRef && inputRef.current) {
+      inputRef.current.value = '';
+      inputRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    document.onclick = (evt: MouseEvent) => {
+      if (inputRef && inputRef.current && evt.target) {
+        if (document.contains(inputRef.current) && evt.target !== inputRef.current) {
+          setStyle({
+            visibility: 'hidden',
+            opacity: 0,
+          });
+          setStyleButton({
+            display: 'none',
+          });
+        }
+      }
+    };
+    // window.onclick = (evt: MouseEvent) => {
+    //   if (inputRef && inputRef.current) {
+    //     if (evt.target.contains(inputRef.current) && evt.target !== inputRef.current) {
+    //       // console.log(`You clicked Outside the box!`);
+    //     }
+    //   }
+    // };
+  }, []);
+
   return (
     <header className="header" id="header">
       <div className="container">
@@ -27,17 +124,13 @@ function Header () {
               <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-lens"></use>
               </svg>
-              <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту"></input>
+              <input ref={inputRef} onChange={handlerInputChangeText} onFocus={handlerOnFocusInput} className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту"></input>
             </label>
-            <ul className="form-search__select-list">
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
-              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
+            <ul className="form-search__select-list" style={style}>
+              {filteredCameras(camerasAll, value).map((camera, tabIndex) => <li key={camera.vendorCode} className="form-search__select-item" tabIndex={tabIndex}><Link to={`/catalog/camera/${camera.id}`}>{camera.name}</Link></li>)}
             </ul>
           </form>
-          <button className="form-search__reset" type="reset">
+          <button onClick={handlerClickResetSearchButton} style={styleButton} className="form-search__reset" type="reset">
             <svg width="10" height="10" aria-hidden="true">
               <use xlinkHref="#icon-close"></use>
             </svg><span className="visually-hidden">Сбросить поиск</span>
@@ -54,3 +147,4 @@ function Header () {
 }
 
 export default Header;
+
