31aca900acb9a090678808fc87b24d70e1279348
Alexander <83508656+kapralovae@users.noreply.github.com>
2022-12-01 14:46:38 +0300

    Update catalog-sort.tsx
    import setCatalogPage

--- src/components/catalog-sort/catalog-sort.tsx
+++ src/components/catalog-sort/catalog-sort.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDisptach } from '../../hooks';
-import { setIsSort, setSortDirection, setSortType } from '../../store/camera-data/camera-data';
+import { setIsSort, setSortDirection, setSortType, setCatalogPage } from '../../store/camera-data/camera-data';
 
 function CatalogSort () {
   const dispatch = useAppDisptach();
